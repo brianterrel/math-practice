93f700411bf9c13b97ab951f443bce45af9f7118
Brian Terrel <brian@localhost.localdomain>
2019-11-20 21:34:25 -0800

    this one is gonna work! still quick and dirty

--- src/math_practice/core.cljs
+++ src/math_practice/core.cljs
@@ -21,37 +21,82 @@
          den denominator
          div 2]
     (cond
-      (or (> div (/ num 2)) (> div (/ den 2))) {:numerator num :denominator den}
+      (or (> div num) (> div den )) {:numerator num :denominator den}
       (common-divisor? num den div) (recur (/ num div) (/ den div) div)
       :else (recur num den (inc div)))))
+
+(defn submit-button-handler
+  "Handle clicks of the submit button by checking the answer etc"
+  [state]
+  (if (and (= false (:submitted @state) ) (= false (:show-solution @state)))
+    (if (= (:final @state) (:answer @state))
+      (swap! state update-in [:streak] inc)
+      (swap! state assoc :streak 0))
+    nil)
+  (swap! state assoc :submitted true))
+
+
+
+
 
 ;Problem generators
 ;
 ;
 (defn fraction-addition [some-atom]
   (let [[a b c d] (take 4 (repeatedly #(inc (rand-int 9))))
-        reduced (reduce-fraction (+ (* a d) (* c b)) (* b d))]
+        reduced (reduce-fraction (+ (* a d) (* c b)) (* b d))
+        fraction (str(:numerator reduced) "/" (:denominator reduced))
+        steps (str "(" a "x" d " + " b "x" c ")/(" b "x" d ")" " = " (str (+ (* a d) (* c b))) "/" (str (* b d)) " = " fraction )]
     (swap! some-atom assoc :show-solution false)
     (swap! some-atom assoc :question (str a "/" b " + " c "/" d " = "))
-    (swap! some-atom assoc :solution (str (:numerator reduced) "/" (:denominator reduced)))))
+    (cond 
+      (= (:numerator reduced) (:denominator reduced)) (do 
+                                                        (swap! some-atom assoc :solution (str steps " = 1"))
+                                                        (swap! some-atom assoc :final "1"))
+      (= 1 (:denominator reduced)) (do 
+                                     (swap! some-atom assoc :solution (str steps " = " (:numerator reduced)))
+                                     (swap! some-atom assoc :final (:numerator reduced)))
+      :else (do 
+              (swap! some-atom assoc :solution steps)
+              (swap! some-atom assoc :final fraction)))))
   
+(defn new-problem-button-handler
+  "Handle clicks oft he new problem button by updating the app state"
+  [state]
+  (fraction-addition state)
+  (swap! state assoc :submitted false))
 
 ;Dat. Page. Content.
 ;
 ;
+(defn answer-input [value]
+  [:input {:type "text"
+           :value (:answer @value)
+           :on-change #(swap! value assoc :answer (-> % .-target .-value))}])
+
 (defn page []
   (let [page-state (r/atom {:question "" 
                             :solution ""
+                            :final ""
                             :streak 0
                             :show-solution false
+                            :submitted false
+                            :answer ""
                             })]
-    (fraction-addition page-state)
+    (new-problem-button-handler page-state)
     (fn []
       [:div
-       [:p (:question @page-state) ]
-       (if (:show-solution @page-state) [:p (:solution @page-state)] [:p ])
+       [:p (str "Correct answers: " (:streak @page-state))]
+       [:p (:question @page-state) 
+        [answer-input page-state]
+        [:input {:type "button" :value "Submit"
+                 :on-click #(submit-button-handler page-state)}]]
+       (if
+         (:show-solution @page-state)
+         [:p (:solution @page-state)]
+         [:p])
        [:input {:type "button" :value "New Problem"
-                :on-click #(fraction-addition page-state)}]
+                :on-click #(new-problem-button-handler page-state)}]
        [:input {:type "button" :value "Show Solution"
                 :on-click #(swap! page-state assoc :show-solution true)}]])))
 
